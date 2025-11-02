313b942307b08e5a5e830c0eda39dd69f16251cb
gangireddysr <gangireddy.sr96@gmail.com>
2025-11-02 12:30:05 +0530

    common

--- shipping.sh
+++ shipping.sh
@@ -10,6 +10,38 @@ systemd_setup
 
 dnf install mysql -y  &>>$LOG_FILE
 
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "User already exist ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading shipping application"
+
+cd /app 
+VALIDATE $? "Changing to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/shipping.zip &>>$LOG_FILE
+VALIDATE $? "unzip shipping"
+
+mvn clean package  &>>$LOG_FILE
+mv target/shipping-1.0.jar shipping.jar 
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+systemctl daemon-reload
+systemctl enable shipping  &>>$LOG_FILE
+
+dnf install mysql -y  &>>$LOG_FILE
+
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
